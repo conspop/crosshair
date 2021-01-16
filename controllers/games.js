065d565d700b05915ab5183c7ca970db8006b30d
sebbby <seb.beitel@gmail.com>
2021-01-16 14:48:01 -0500

    Refactor calculateHandValue() function

--- controllers/games.js
+++ controllers/games.js
@@ -5,14 +5,14 @@ module.exports = {
   joinGame,
   playCard,
   refresh,
-  checkHand
+  calculateHandValue
 };
 
 async function newGame(req, res) {
   const shuffledDeck = createShuffledDeck()
   
   const board = Array(25).fill('')
-  // board[12] = shuffledDeck.pop()
+  board[12] = shuffledDeck.pop()
 
   console.log(board)
 
@@ -21,7 +21,7 @@ async function newGame(req, res) {
     playerOneName: req.body.username,
     playerOneHand: dealCards(shuffledDeck, 12),
     playerTwoHand: dealCards(shuffledDeck, 12),
-    board: dealCards(shuffledDeck, 25),
+    board,
     turn: true
   })
 
@@ -105,7 +105,7 @@ async function playCard(req, res) {
   }
   game.set({board: req.body.board})
   game.turn = !game.turn
-  checkWinner(game)
+  // Going to need to change a property of game for scoreboard
   await game.save()
   res.json(game)
 }
@@ -115,195 +115,254 @@ async function refresh(req, res) {
   res.json(game)
 }
 
-function checkWinner({board}) {
-  if (board.includes('')) {
-    console.log('game is not over!')
-    return
-  } else {
-    checkHand([
-      board[0],
-      board[1],
-      board[2],
-      board[3],
-      board[4]
-    ])
+
+
+function buildScoreboard({board}) {
+  const playerOneHandIndexesArray = [
+    [0,5,10,15,20],
+    [1,6,11,16,21],
+    [2,7,12,17,22],
+    [3,8,13,18,23],
+    [4,9,14,19,24]
+  ]
+  const playerTwoHandIndexesArray = [
+    [20,21,22,23,24],
+    [15,16,17,18,19],
+    [10,11,12,13,14],
+    [5,6,7,8,9],
+    [0,1,2,3,4]
+  ]
+  
+  return scoreboard = {
+    playerOne: calculateHandValues(board, playerOneHandIndexesArray),
+    playerTwo: calculateHandValues(board, playerTwoHandIndexesArray)
   }
 }
 
-function checkHand(hand) {
+function calculateHandValues(board, handIndexesArray) {
+  const handValues = []
+  handIndexesArray.forEach(handIndexArray => {
+    const hand = makeHand(board, handIndexArray)
+    handValues.push(calculateHandValue(hand))
+  })
+  return handValues
+}
+
+function makeHand(board, handIndexArray) {
+  const hand = []
+  handIndexArray.forEach(handIndex => {
+    hand.push(board[handIndex])
+  })
+  console.log(hand)
   hand.sort((a,b) => b.number - a.number)
-  
-  return checkRoyalFlush(hand)
+  console.log(hand)
+  return hand
+}
 
-  function checkRoyalFlush(hand) {  
-    const isFlush = areAllSameSuit(hand)
-    const isStraight = areStraight(hand)
-    
-    if (
-      isFlush &&
-      isStraight &&
-      hand[0].number === 14
-    ) {
-      const handValue = [10]
-      return handValue
-    }
-    return checkStraightFlush(hand)
+function calculateHandValue(hand) {
+  let handValue
+  if (isRoyalFlush(hand)) {
+    handValue = [10]
+  } else if (isStraightFlush(hand)) {
+    handValue = [9,hand[0].number]
+  } else if (is4OfAKind(hand)) {
+    handValue = get4OfAKindValue(hand)
+  } else if (isFullHouse(hand)) {
+    handValue = getFullHouseValue(hand)
+  } else if (isFlush(hand)) {
+    const numbersArray = hand.map(card => card.number)
+    handValue = [6, ...numbersArray]
+  } else if (isStraight(hand)) {
+    handValue = [5, hand[0].number]
+  } else if (is3OfAKind(hand)) {
+    handValue = get3OfAKindValue(hand)
+  } else if (is2Pair(hand)) {
+    handValue = get2PairValue(hand)
+  } else if (isPair(hand)) {
+    handValue = getPairValue(hand)
+  } else {
+    handValue = [1, ...hand.map(card => card.number)]
   }
+  return handValue
+}
 
-  function checkStraightFlush(hand) {
-    const isFlush = areAllSameSuit(hand)
-    const isStraight = areStraight(hand)
-    
-    if (
-      isFlush &&
-      isStraight
-    ) {
-      const handValue = [9,hand[0].number]
-      return handValue
-    }
-    return check4OfAKind(hand)
-  }
 
-  function check4OfAKind(hand) {
-    const set1 = hand.slice(0,4)
-    const set2 = hand.slice(1)
-    
-    if (areSameNumber(set1)) {
-      const handValue = [8, set1[0].number, hand[4].number]
-      return handValue
-    } else if (areSameNumber(set2)) {
-      const handValue = [8, set2[0].number, hand[0].number]
-      return handValue
-    }
-    return checkFullHouse(hand)
-  }
+function isRoyalFlush(hand) {  
+  const isFlush = areAllSameSuit(hand)
+  const isStraight = areStraight(hand)
+  const highCard = hand[0].number
+  return (
+    isFlush && 
+    isStraight &&
+    highCard === 14
+  )
+}
 
-  function checkFullHouse(hand) {
-    const set1 = [hand.slice(0,3), hand.slice(3)]
-    const set2 = [hand.slice(2), hand.slice(0,2)]
+function isStraightFlush(hand) {
+  const isFlush = areAllSameSuit(hand)
+  const isStraight = areStraight(hand)
+  return (
+    isFlush &&
+    isStraight
+  )
+}
 
-    if (areSameNumber(set1[0])) {
-      if (areSameNumber(set1[1])) {
-        const handValue = [7, set1[0][0].number, set1[1][0].number]
-        return handValue
-      }
-    }
-    if (areSameNumber(set2[0])) {
-      if (areSameNumber(set2[1])) {
-        const handValue = [7, set2[0][0].number, set2[1][0].number]
-        return handValue
-      }
-    }
-    return checkFlush(hand)
-  }
+function is4OfAKind(hand) {
+  const set1 = hand.slice(0,4)
+  const set2 = hand.slice(1)
+  return (
+    areSameNumber(set1) ||
+    areSameNumber(set2)
+  )
+}
 
-  function checkFlush(hand) {
-    if (areAllSameSuit(hand)) {
-      const numbersArray = hand.map(card => card.number)
-      const handValue = [6, ...numbersArray]
-      return handValue
-    }
-    return checkStraight(hand)
-  }
-
-  function checkStraight(hand) {
-    if (areStraight(hand)) {
-      const handValue = [5, hand[0].number]
-      return handValue
-    }
-    return check3OfAKind(hand)
-  }
-
-  function check3OfAKind(hand) {
-    const set1 = hand.slice(0,3)
-    const set2 = hand.slice(1,4)
-    const set3 = hand.slice(2)
-
-    if (areSameNumber(set1)) {
-      const handValue = [4, set1[0].number, hand[3].number, hand[4].number]
-      return handValue
-    }
-    if (areSameNumber(set2)) {
-      const handValue = [4, set2[0].number, hand[0].number, hand[4].number]
-      return handValue
-    }
-    if (areSameNumber(set3)) {
-      const handValue = [4, set3[0].number, hand[0].number, hand[1].number]
-      return handValue
-    }
-    return check2Pair(hand)
-  }
-
-  function check2Pair(hand) {
-    let pairs = []
-    let single
-
-    for (let index = 0; index <= 4; index++) {
-      if (index < 4 && hand[index].number === hand[index + 1].number) {
-        pairs.push(hand[index].number)
-      } else if (index > 0 && hand[index].number !== hand[index -1].number) {
-        single = hand[index].number
-      } else if (index === 0 && hand[index].number !== hand[index + 1].number) {
-        single = hand[index].number
-      }
-    }
-
-    if (pairs.length === 2) {
-      const handValue = [3, ...pairs, single]
-      return handValue
-    }
-    return checkPair(hand)
-  }
-
-  function checkPair(hand) {
-    const set1 = [hand.slice(0,2), hand.slice(2)]
-    const set2 = [hand.slice(1,3), [hand[0].number, hand[3], hand[4]]]
-    const set3 = [hand.slice(2,4), [hand[0], hand[1], hand[4]]]
-    const set4 = [hand.slice(3), hand.slice(0,3)]
-
-    if (areSameNumber(set1[0])) {
-      const handValue = [2, set1[0][0].number, ...set1[1].map(card => card.number)]
-      return handValue
-    }
-    if (areSameNumber(set2[0])) {
-      const handValue = [2, set2[0][0].number, ...set2[1].map(card => card.number)]
-      return handValue
-    }
-    if (areSameNumber(set3[0])) {
-      const handValue = [2, set3[0][0].number, ...set3[1].map(card => card.number)]
-      return handValue
-    }
-    if (areSameNumber(set4[0])) {
-      const handValue = [2, set4[0][0].number, ...set4[1].map(card => card.number)]
-      return handValue
-    }
-
-    // If no pair, return high cards
-    const handValue = [...hand.map(card => card.number)]
+function get4OfAKindValue(hand) {
+  const set1 = hand.slice(0,4)
+  const set2 = hand.slice(1)
+  if (areSameNumber(set1)) {
+    const handValue = [8, set1[0].number, hand[4].number]
+    return handValue
+  } else if (areSameNumber(set2)) {
+    const handValue = [8, set2[0].number, hand[0].number]
     return handValue
   }
-
-  function areAllSameSuit(handArray) {
-    const suits = handArray.map(card => card.suit)
-    return suits.every(suit => suit === suits[0])
-  }
-
-  function areStraight(hand) {
-    return (
-      hand[0].number - 1 === hand[1].number &&
-      hand[1].number - 1 === hand[2].number &&
-      hand[2].number - 1 === hand[3].number &&
-      hand[3].number - 1 === hand[4].number
-    )
-  }
-
-  function areSameNumber(set) {
-    return set.every(card => card.number === set[0].number)
-  }
 }
 
+function isFullHouse(hand) {
+  const set1 = [hand.slice(0,3), hand.slice(3)]
+  const set2 = [hand.slice(2), hand.slice(0,2)]
+  return (
+    (areSameNumber(set1[0]) && areSameNumber(set1[1])) ||
+    (areSameNumber(set2[0]) && areSameNumber(set2[1]))
+  )
+}
 
+function getFullHouseValue(hand) {
+  const set1 = [hand.slice(0,3), hand.slice(3)]
+  const set2 = [hand.slice(2), hand.slice(0,2)]
+  let handValue
+  if (areSameNumber(set1[0]) && areSameNumber(set1[1])) {
+    handValue = [7, set1[0][0].number, set1[1][0].number]
+  } else if (areSameNumber(set2[0]) && areSameNumber(set2[1])) {
+    handValue = [7, set2[0][0].number, set2[1][0].number]
+  }
+  return handValue
+}
 
+function isFlush(hand) {
+  return areAllSameSuit(hand)
+}
 
+function isStraight(hand) {
+  return areStraight(hand)
+}
 
+function is3OfAKind(hand) {
+  const set1 = hand.slice(0,3)
+  const set2 = hand.slice(1,4)
+  const set3 = hand.slice(2)
+  return (
+    areSameNumber(set1) ||
+    areSameNumber(set2) ||
+    areSameNumber(set3)
+  )
+}
 
+function get3OfAKindValue(hand) {
+  const set1 = hand.slice(0,3)
+  const set2 = hand.slice(1,4)
+  const set3 = hand.slice(2)
+  let handValue
+  if (areSameNumber(set1)) {
+    handValue = [4, set1[0].number, hand[3].number, hand[4].number]
+  } else if (areSameNumber(set2)) {
+    handValue = [4, set2[0].number, hand[0].number, hand[4].number]
+  } else if (areSameNumber(set3)) {
+    handValue = [4, set3[0].number, hand[0].number, hand[1].number]
+  }
+  return handValue
+}
+
+function is2Pair(hand) {
+  let pairs = []
+  let single
+  for (let index = 0; index <= 4; index++) {
+    if (index < 4 && hand[index].number === hand[index + 1].number) {
+      pairs.push(hand[index].number)
+    } else if (index > 0 && hand[index].number !== hand[index -1].number) {
+      single = hand[index].number
+    } else if (index === 0 && hand[index].number !== hand[index + 1].number) {
+      single = hand[index].number
+    }
+  }
+  return (pairs.length === 2)
+}
+
+function get2PairValue(hand) {
+  let pairs = []
+  let single
+
+  for (let index = 0; index <= 4; index++) {
+    if (index < 4 && hand[index].number === hand[index + 1].number) {
+      pairs.push(hand[index].number)
+    } else if (index > 0 && hand[index].number !== hand[index -1].number) {
+      single = hand[index].number
+    } else if (index === 0 && hand[index].number !== hand[index + 1].number) {
+      single = hand[index].number
+    }
+  }
+  const handValue = [3, ...pairs, single]
+  return handValue
+}
+
+function isPair(hand) {
+  const set1 = [hand.slice(0,2), hand.slice(2)]
+  const set2 = [hand.slice(1,3), [hand[0].number, hand[3], hand[4]]]
+  const set3 = [hand.slice(2,4), [hand[0], hand[1], hand[4]]]
+  const set4 = [hand.slice(3), hand.slice(0,3)]
+  return (
+    areSameNumber(set1[0]) ||
+    areSameNumber(set2[0]) ||
+    areSameNumber(set3[0]) ||
+    areSameNumber(set4[0])
+  )
+}
+
+function getPairValue(hand) {
+  const set1 = [hand.slice(0,2), hand.slice(2)]
+  const set2 = [hand.slice(1,3), [hand[0].number, hand[3], hand[4]]]
+  const set3 = [hand.slice(2,4), [hand[0], hand[1], hand[4]]]
+  const set4 = [hand.slice(3), hand.slice(0,3)]
+  let handValue
+  if (areSameNumber(set1[0])) {
+    handValue = [2, set1[0][0].number, ...set1[1].map(card => card.number)]
+  }
+  if (areSameNumber(set2[0])) {
+    handValue = [2, set2[0][0].number, ...set2[1].map(card => card.number)]
+  }
+  if (areSameNumber(set3[0])) {
+    handValue = [2, set3[0][0].number, ...set3[1].map(card => card.number)]
+  }
+  if (areSameNumber(set4[0])) {
+    handValue = [2, set4[0][0].number, ...set4[1].map(card => card.number)]
+  }
+  return handValue
+}
+
+function areAllSameSuit(handArray) {
+  const suits = handArray.map(card => card.suit)
+  return suits.every(suit => suit === suits[0])
+}
+
+function areStraight(hand) {
+  return (
+    hand[0].number - 1 === hand[1].number &&
+    hand[1].number - 1 === hand[2].number &&
+    hand[2].number - 1 === hand[3].number &&
+    hand[3].number - 1 === hand[4].number
+  )
+}
+
+function areSameNumber(set) {
+  return set.every(card => card.number === set[0].number)
+}
--- controllers/games.test.js
+++ controllers/games.test.js
@@ -1,79 +1,79 @@
-const {checkHand} = require('./games')
+const {calculateHandValue} = require('./games')
 
 test('Royal flush should return [10]', () => {
-  expect(checkHand([
+  expect(calculateHandValue([
     {number: 14, suit: 'S'},
-    {number: 11, suit: 'S'},
+    {number: 13, suit: 'S'},
     {number: 12, suit: 'S'},
-    {number: 10, suit: 'S'},
-    {number: 13, suit: 'S'}
+    {number: 11, suit: 'S'},
+    {number: 10, suit: 'S'}
   ])).toEqual([10])
 })
 
 test('Straight Flush should return [9, (highest card)]', () => {
-  expect(checkHand([
-    {number: 9, suit: 'S'},
-    {number: 11, suit: 'S'},
+  expect(calculateHandValue([
+    {number: 13, suit: 'S'},
     {number: 12, suit: 'S'},
+    {number: 11, suit: 'S'},
     {number: 10, suit: 'S'},
-    {number: 13, suit: 'S'}
+    {number: 9, suit: 'S'}
   ])).toEqual([9,13])
 })
 
 test('4 Of A Kind should return [8,(set card),(high card)]', () => {
-  expect(checkHand([
+  expect(calculateHandValue([
     {number: 9, suit: 'S'},
     {number: 9, suit: 'S'},
-    {number: 7, suit: 'S'},
     {number: 9, suit: 'S'},
-    {number: 9, suit: 'S'}
+    {number: 9, suit: 'S'},
+    {number: 7, suit: 'S'}
   ])).toEqual([8,9,7])
 })
 
 test('4 Of A Kind should return [8,(set card),(high card)]', () => {
-  expect(checkHand([
-    {number: 9, suit: 'S'},
-    {number: 9, suit: 'S'},
+  expect(calculateHandValue([
     {number: 10, suit: 'S'},
+    {number: 9, suit: 'S'},
+    {number: 9, suit: 'S'},
     {number: 9, suit: 'S'},
     {number: 9, suit: 'S'}
   ])).toEqual([8,9,10])
 })
 
 test('Full House should return [7,(3 set card),(2 set card)]', () => {
-  expect(checkHand([
-    {number: 9, suit: 'S'},
-    {number: 9, suit: 'S'},
+  expect(calculateHandValue([
+    {number: 10, suit: 'S'},
     {number: 10, suit: 'S'},
     {number: 9, suit: 'S'},
-    {number: 10, suit: 'S'}
+    {number: 9, suit: 'S'},
+    {number: 9, suit: 'S'}
   ])).toEqual([7,9,10])
 })
 
 test('Flush should return [6,(5 numbers ranked descending)]', () => {
-  expect(checkHand([
+  expect(calculateHandValue([
+    {number: 13, suit: 'S'},
     {number: 9, suit: 'S'},
     {number: 6, suit: 'S'},
     {number: 4, suit: 'S'},
-    {number: 2, suit: 'S'},
-    {number: 13, suit: 'S'}
+    {number: 2, suit: 'S'}
   ])).toEqual([6,13,9,6,4,2])
 })
 
 test('Straight should return [5,(high card)]', () => {
-  expect(checkHand([
-    {number: 3, suit: 'S'},
+  expect(calculateHandValue([
+    {number: 7, suit: 'S'},
     {number: 6, suit: 'C'},
-    {number: 4, suit: 'S'},
-    {number: 7, suit: 'D'},
-    {number: 5, suit: 'S'}
+    {number: 5, suit: 'S'},
+    {number: 4, suit: 'D'},
+    {number: 3, suit: 'S'}
   ])).toEqual([5,7])
 })
 
 test('3 Of A Kind should return [4,(set card),(high card),(next high card)]', () => {
-  expect(checkHand([
-    {number: 7, suit: 'S'},
-    {number: 12, suit: 'C'},
+  expect(calculateHandValue([
+    {number: 12, suit: 'S'},
+    {number: 7, suit: 'C'},
     {number: 7, suit: 'S'},
     {number: 7, suit: 'D'},
     {number: 5, suit: 'S'}
@@ -81,61 +81,61 @@ test('3 Of A Kind should return [4,(set card),(high card),(next high card)]', ()
 })
 
 test('3 Of A Kind should return [4,(set card),(high card),(next high card)]', () => {
-  expect(checkHand([
+  expect(calculateHandValue([
     {number: 7, suit: 'S'},
-    {number: 2, suit: 'C'},
+    {number: 7, suit: 'C'},
     {number: 7, suit: 'S'},
-    {number: 7, suit: 'D'},
-    {number: 5, suit: 'S'}
+    {number: 5, suit: 'D'},
+    {number: 2, suit: 'S'}
   ])).toEqual([4,7,5,2])
 })
 
 test('3 Of A Kind should return [4,(set card),(high card),(next high card)]', () => {
-  expect(checkHand([
-    {number: 7, suit: 'S'},
-    {number: 12, suit: 'C'},
+  expect(calculateHandValue([
+    {number: 12, suit: 'S'},
+    {number: 10, suit: 'C'},
     {number: 7, suit: 'S'},
     {number: 7, suit: 'D'},
-    {number: 10, suit: 'S'}
+    {number: 7, suit: 'S'}
   ])).toEqual([4,7,12,10])
 })
 
 test('2 Pair should return [3,(high set),(low set),(high card)]', () => {
-  expect(checkHand([
-    {number: 7, suit: 'S'},
-    {number: 12, suit: 'C'},
+  expect(calculateHandValue([
+    {number: 12, suit: 'S'},
+    {number: 10, suit: 'C'},
     {number: 10, suit: 'S'},
     {number: 7, suit: 'D'},
-    {number: 10, suit: 'S'}
+    {number: 7, suit: 'S'}
   ])).toEqual([3,10,7,12])
 })
 
 test('2 Pair should return [3,(high set),(low set),(high card)]', () => {
-  expect(checkHand([
-    {number: 7, suit: 'S'},
-    {number: 8, suit: 'C'},
+  expect(calculateHandValue([
     {number: 10, suit: 'S'},
+    {number: 10, suit: 'C'},
+    {number: 8, suit: 'S'},
     {number: 7, suit: 'D'},
-    {number: 10, suit: 'S'}
+    {number: 7, suit: 'S'}
   ])).toEqual([3,10,7,8])
 })
 
 test('Pair should return [2,(set),(ranked high cards)]', () => {
-  expect(checkHand([
-    {number: 7, suit: 'S'},
-    {number: 8, suit: 'C'},
+  expect(calculateHandValue([
     {number: 11, suit: 'S'},
+    {number: 10, suit: 'C'},
+    {number: 8, suit: 'S'},
     {number: 7, suit: 'D'},
-    {number: 10, suit: 'S'}
+    {number: 7, suit: 'S'}
   ])).toEqual([2,7,11,10,8])
 })
 
 test('High card should return [1,(ranked high cards)]', () => {
-  expect(checkHand([
-    {number: 7, suit: 'S'},
-    {number: 8, suit: 'C'},
+  expect(calculateHandValue([
     {number: 11, suit: 'S'},
-    {number: 2, suit: 'D'},
-    {number: 10, suit: 'S'}
-  ])).toEqual([11,10,8,7,2])
+    {number: 10, suit: 'C'},
+    {number: 8, suit: 'S'},
+    {number: 7, suit: 'D'},
+    {number: 2, suit: 'S'}
+  ])).toEqual([1,11,10,8,7,2])
 })

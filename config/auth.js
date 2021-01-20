ec1255f33b267130287fca8d129f0fd44a3a22ec
sebbby <seb.beitel@gmail.com>
2021-01-20 08:26:23 -0500

    test token on production

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,11 @@ const SECRET = process.env.SECRET;
 
 module.exports = function(req, res, next) {
   // Check for the token being sent in three different ways
-  let token = req.get('Authorization') || req.query.token || req.body.token || req.data.token;
+  console.log(req.get('Authorization'))
+  console.log(req.query.token)
+  console.log(req.body.token)
+
+  let token = req.get('Authorization') || req.query.token || req.body.token;
   if (token) {
     // Remove the 'Bearer ' if it was included in the token header
     token = token.replace('Bearer ', '');

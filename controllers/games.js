471fbb29e2a5b648039e4a0248bad9fa9976c3c3
sebbby <seb.beitel@gmail.com>
2021-02-01 15:36:34 -0500

    Change version number

--- controllers/games.js
+++ controllers/games.js
@@ -17,7 +17,7 @@ module.exports = {
 
 // this adjusts how big the swing for wins and losses
 KFACTOR = 32
-VERSION = '2.2 - Resign Functionality'
+VERSION = '2.3 - Working Resign Functionality'
 
 async function updateVersion(req, res) {
   const user = await User.findById(req.user._id)
